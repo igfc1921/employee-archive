5aed58221ad52e5152b1941f9ac1399b92de05d0
igfc1921 <humamalhassnawi1@gmail.com>
2026-08-23 14:50:19 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 // ⚠️ عبّي هذا الملف بمعلوماتك من Google Cloud Console (راجع README.md للتفاصيل)
 
 export const CLIENT_ID = '745242218621-53sd6hj1vpl3917av9523bua3kjq5p8i.apps.googleusercontent.com';
-export const API_KEY = '
-AIzaSyAArP2fembq72MOavyjcjptILti23i-rXU';
+export const API_KEY = 'AIzaSyAArP2fembq72MOavyjcjptILti23i-rXU';
 
 // أذونات الوصول المطلوبة من جوجل درايف
 // drive.file: يعطي الموقع صلاحية فقط على الملفات/المجلدات اللي المستخدم يختارها بنفسه أو ينشئها الموقع
